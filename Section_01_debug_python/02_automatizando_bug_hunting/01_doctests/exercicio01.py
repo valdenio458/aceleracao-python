4b51e6118f08229d89ee047eeb30b7fdfda53a8f
valdenio458 <valdeniomartinho@gmail.com>
2023-11-23 15:00:54 -0300

    Exercício 01

---
+++ Section_01_debug_python/02_automatizando_bug_hunting/01_doctests/exercicio01.py
@@ -0,0 +1,14 @@
+def sum_two_numbers(a, b):
+    """Retorna a soma de dois números recebidos por parâmetro.
+
+    Exemplos
+    --------
+    >>> sum_two_numbers(0, 0)
+    0
+    >>> sum_two_numbers(2, 4)
+    6
+    """
+    return a + b  # Bug corrigido
+
+
+print(sum_two_numbers(2, 4))
