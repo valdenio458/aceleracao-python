01f1fb5f35955f40cf9d1ac4fdd27f0e81e351ac
valdenio458 <valdeniomartinho@gmail.com>
2023-11-24 16:36:15 -0300

    Fixtures builtin

---
+++ Section_01_debug_python/02_automatizando_bug_hunting/03_fixtures/fixtures_test.py
@@ -0,0 +1,14 @@
+# Sim, é só receber `capsys` como parâmetro em qualquer função de teste que o
+# pytest faz o resto da magia acontecer
+def test_print_to_stdout(capsys):
+    print("Hello, world!")
+    captured = capsys.readouterr()
+    assert captured.out == "Hello, world!\n"  # print coloca \n automaticamente
+
+
+def test_error_to_stderr(capsys):
+    import sys
+
+    sys.stderr.write("Error message\n")
+    captured = capsys.readouterr()
+    assert captured.err == "Error message\n"
---
+++ Section_01_debug_python/02_automatizando_bug_hunting/03_fixtures/monkeypatch_test.py
@@ -0,0 +1,14 @@
+def my_function():
+    return f"Você digitou {input('Digite algo: ')}!"
+
+
+def test_my_function(monkeypatch):
+    # Input recebe um parâmetro que mock_input não usa, por isso o _
+    def mock_input(_):
+        return "Python"
+
+    # Trocamos a input do sistema pela nossa mock_input
+    monkeypatch.setattr("builtins.input", mock_input)
+    output = my_function()
+
+    assert output == "Você digitou Python!"
